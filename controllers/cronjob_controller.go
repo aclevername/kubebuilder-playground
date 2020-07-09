702dd3f142c7e8ede01bc50e6ed690d2010f5eba
Jake Klein <jake@ori.co>
2020-07-09 14:35:21 +0100

    Rename package to same as example tutorial.kubebuilder.io/project

--- controllers/cronjob_controller.go
+++ controllers/cronjob_controller.go
@@ -36,7 +36,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	batch "github.com/aclevername/kubebuilder-playground/api/v1"
+	batch "tutorial.kubebuilder.io/project/api/v1"
 )
 
 /*
